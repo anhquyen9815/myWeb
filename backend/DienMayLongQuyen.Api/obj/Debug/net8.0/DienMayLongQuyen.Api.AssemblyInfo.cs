d90be9a6f8878e48be34941fa401a95f55ca5829
IT <IT@Tuans-Mac-mini.local>
2025-11-22 16:02:20 +0700

    cap nhat code

--- backend/DienMayLongQuyen.Api/obj/Debug/net8.0/DienMayLongQuyen.Api.AssemblyInfo.cs
+++ backend/DienMayLongQuyen.Api/obj/Debug/net8.0/DienMayLongQuyen.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DienMayLongQuyen.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f52ee096e64b4cb750780823486651cd0b0f11e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be42a07a282e7369521bf1b6e57f42c87b2f4e03")]
 [assembly: System.Reflection.AssemblyProductAttribute("DienMayLongQuyen.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DienMayLongQuyen.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
